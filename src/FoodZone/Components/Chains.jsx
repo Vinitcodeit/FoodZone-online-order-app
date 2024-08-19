bae6650c12cfdc14bfcf093249bf9953f28d154f
Vinitcodeit <vtambralli412@gmail.com>
2024-08-19 20:20:34 +0530

    added media queries for the responsive web site

--- src/FoodZone/Components/Chains.jsx
+++ src/FoodZone/Components/Chains.jsx
@@ -1,82 +1,107 @@
-import React, { useEffect, useState } from 'react'
-import { API_URL } from '../api'
+import React, { useEffect, useState } from "react";
+import { API_URL } from "../api";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { FaRegArrowAltCircleLeft } from "react-icons/fa";
+import { MagnifyingGlass } from "react-loader-spinner";
 
 const Chains = () => {
-    const [vendorData, setVendorData] = useState([])
-    const [scroll, setScroll] = useState(0)
+  const [vendorData, setVendorData] = useState([]);
+  const [scroll, setScroll] = useState(0);
+  const [loading, setLoading] = useState(true);
 
-    const vendorFirmHandler = async()=>{
-try {
-    const response = await fetch(`${API_URL}/vendor/all-vendors`)
+  const vendorFirmHandler = async () => {
+    try {
+      const response = await fetch(`${API_URL}/vendor/all-vendors`);
 
-    const newData = await response.json()
-    setVendorData(newData)
-    console.log("this is api data", newData);
-    
-} catch (error) {
-    alert("Failed to fetch data")
-    console.error("Failed to fetch data");
-    
-}
-}
+      const newData = await response.json();
+      setVendorData(newData);
+      console.log("this is api data", newData);
+      setLoading(false)
+    } catch (error) {
+      alert("Failed to fetch data");
+      console.error("Failed to fetch data");
+      setLoading(true)
+    }
+  };
 
-useEffect(()=>{
-  vendorFirmHandler()
-},[])
+  useEffect(() => {
+    vendorFirmHandler();
+  }, []);
 
- const handleScroll = (direction)=>{
- const gallery = document.getElementById('chainGallery')
- const scrollAmount = 400;
-  
- if(direction === "left"){
-  gallery.scrollTo({
-    left:gallery.scrollLeft - scrollAmount,
-    behavior: "smooth"
-  })
- }else if(direction === "right"){
-  gallery.scrollTo({
-    left: gallery.scrollLeft + scrollAmount,
-    behavior: "smooth"
-  })
- }
-}
+  const handleScroll = (direction) => {
+    const gallery = document.getElementById("chainGallery");
+    const scrollAmount = 400;
+
+    if (direction === "left") {
+      gallery.scrollTo({
+        left: gallery.scrollLeft - scrollAmount,
+        behavior: "smooth",
+      });
+    } else if (direction === "right") {
+      gallery.scrollTo({
+        left: gallery.scrollLeft + scrollAmount,
+        behavior: "smooth",
+      });
+    }
+  };
 
   return (
-    <>
-    <div className="btnSection">
-      <button onClick={()=>handleScroll("left")} >Left</button>
-      <button onClick={()=>handleScroll("right")}>Right</button>
-    </div>
-    <h3>Top restaurant chains in Bangalore</h3>
-<section className='chainSection' id='chainGallery' onScroll={(e)=>setScroll(e.target.scrollLeft)}>
-    {vendorData.vendors && vendorData.vendors.map((vendor)=>{
-      return(
+    <div className="mediaChainSection">
+     <div className="loaderSection">
+     {loading && (
         <>
-          <div className="vendorBox">
-            {vendor.firm.map((item)=>{
-              return(
-                <>
-                <div>
-                <strong>
-                {item.firmName}
-                </strong>
-        
-                </div>
-                <div className="firmImage">
-                  <img src={`${API_URL}/uploads/${item.image}`} />
-                </div>
-                </>
-              )
-            })}
-          </div>
+          <div className="loader">Your 🥗 is Loading... </div>
+          <MagnifyingGlass
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="magnifying-glass-loading"
+            wrapperStyle={{}}
+            wrapperClass="magnifying-glass-wrapper"
+            glassColor="#c0efff"
+            color="#e15b64"
+          />
         </>
-      )
-    })}
-         
-   </section>
-    </>
-   
-  )
-}
+      )}
+     </div>
+      <div className="btnSection">
+        <button onClick={() => handleScroll("left")}>
+          <FaRegArrowAltCircleLeft className="btnIcons" />
+        </button>
+        <button onClick={() => handleScroll("right")}>
+          <FaRegArrowAltCircleRight className="btnIcons" />
+        </button>
+      </div>
+      <h3>Top restaurant chains in Bangalore</h3>
+      <section
+        className="chainSection"
+        id="chainGallery"
+        onScroll={(e) => setScroll(e.target.scrollLeft)}
+      >
+        {vendorData.vendors &&
+          vendorData.vendors.map((vendor) => {
+            return (
+              <>
+                <div className="vendorBox">
+                  {vendor.firm.map((item) => {
+                    return (
+                      <>
+                        <div>
+                          <strong>{item.firmName}</strong>
+                        </div>
+                        <div className="firmImage">
+                          <img src={`${API_URL}/uploads/${item.image}`} />
+                        </div>
+                      </>
+                    );
+                  })}
+                </div>
+              </>
+            );
+          })}
+      </section>
+    </div>
+  );
+};
 
-export default Chains
+export default Chains;
