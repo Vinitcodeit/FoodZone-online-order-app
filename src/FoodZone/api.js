c304fbadeec7bc3fb01ff3032720810e683afe98
Vinitcodeit <vtambralli412@gmail.com>
2024-12-11 08:45:47 +0530

    updated api path

--- src/FoodZone/api.js
+++ src/FoodZone/api.js
@@ -1,3 +1,3 @@
-// export const API_URL = "https://foodzone-backend-nodejs.onrender.com"
-export const API_URL = "http://localhost:4000"
+export const API_URL = "https://foodzone-backend-nodejs.onrender.com"
+// export const API_URL = "http://localhost:4000"
 // export const API_URL = "http://43.204.237.236/"
